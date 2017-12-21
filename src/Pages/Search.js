23b3fc46ac78e9bb5c5828c4456e0bb1e47679c6
tjb1272 <30851416+tjb1272@users.noreply.github.com>
2017-12-21 12:43:15 -0700

    Update Search.js

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,6 +1,61 @@
-import React from 'react'
-import Header from '../Components/Header'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+     
+class Search extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { repos: [] }
+  }
+     
+    getRepos(searchValue) {
 
-const Search = () => <h4><Header title='Search' /></h4>
+      let url = `https://api.github.com/search/repositories?q=${searchValue}&language=javascript`
+      console.log(url)
+        fetch(url).then( (response) => {
+          return response.json();
+        }).then((results) => {
+          if(results.items != undefined){
+            console.log(results.items)
+            let items = results.items   
+            this.setState({ repos: items })
+            }
+          });
+        
+        }
 
+      handleSubmit(event) {
+        event.preventDefault();
+        this.props.onSubmit(this.props.id, this.props.repos);
+      }
+
+    render () {
+        return (
+          <div>
+             <form className='text' onSubmit={this.handleSubmit}>
+             <input
+             id='username'
+             placeholder='github username'
+             value={this.state.username}
+             type='text'
+           />
+           {' '}
+           <button
+             className='button'
+             type='submit'
+           >
+             Submit
+           </button>
+             </form> 
+             {' '}
+            <ul>
+               {this.state.repos.map(i => {
+              return <li>{this.state.repos.items}</li>
+               })} 
+            </ul>
+          </div>
+        )
+      } 
+    };
+  
+    
 export default Search;
