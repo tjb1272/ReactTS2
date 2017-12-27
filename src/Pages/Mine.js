2fe31f4de534cfa4ce5e94d7319c708d2d60ae3e
tjb1272 <30851416+tjb1272@users.noreply.github.com>
2017-12-26 17:09:02 -0700

    Update Mine.js

--- src/Pages/Mine.js
+++ src/Pages/Mine.js
@@ -1,63 +1,14 @@
-import React, { Component } from 'react'
-     
-class Search extends Component {
-    constructor (props) {
-        super(props)
-        this.state = {repos: null }
-    } 
-     
-    getRepos(searchValue) {
+import React from 'react'
 
-      let url = `https://api.github.com/search/repositories?q=${searchValue}&language=javascript`
-      console.log(url)
-        fetch(url).then( (response) => {
-          return response.json();
-        }).then((results) => {
-          if(results.items !== undefined){
-            console.log(results.items)
-            let items = results.items;         
-            this.setState({ repos: items })
-            }
-          });
-        
-        }
+class Mine extends React.Component {
 
-      handleSubmit(event) {
-        event.preventDefault();
-        this.props.onSubmit(this.props.id, this.props.repos);
-      }
-
-    render () {
+    render ()
+    {
         return (
-          <div>
-             <form className='text' onSubmit={this.handleSubmit}>
-             <input
-             id='username'
-             placeholder='github username'
-             value={this.state.username}
-             type='text'
-           />
-           {' '}
-           <button
-             className='button'
-             type='submit'
-           >
-             Submit
-           </button>
-             </form> 
-             {' '}
-            <ul>
-               {this.state.items.map((items) => (
-              <li>
-              {repos.items}
-              </li>
-                ))}
-
-            </ul>
-          </div>
-        )
-      } 
-    };
-  
-    
-export default Search;
+        <div>
+            <h4>Hello There</h4>
+        </div>
+        );
+    }
+    }
+    export default Mine;
