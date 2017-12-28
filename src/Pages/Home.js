ed6ce260cc52c6bae3d699dec447183db26a4e7d
tjb1272 <30851416+tjb1272@users.noreply.github.com>
2017-12-27 20:26:46 -0700

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,8 +5,15 @@ class Home extends React.Component {
 render ()
 {
     return (
-    <h4>Welcome!</h4>
+    <div>
+        <h2>Welcome!</h2>
+            <p>
+                Check out the different pages of my site.<br /> 
+                You can search your repositories or learn to count....Check it Out! 
+            </p>
+    </div>
     );
 }
 }
 export default Home;
+
