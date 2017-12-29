e0707286c60b0dbac42a12c50ffbec1c13b7ce12
tjb1272 <30851416+tjb1272@users.noreply.github.com>
2017-12-28 19:37:48 -0700

    Update Mine.js

--- src/Pages/Mine.js
+++ src/Pages/Mine.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import './me2.jpg'
+
+const mstyle = {
+    fontSize: '48px',
+};
 
 class Mine extends React.Component {
+
+
 
     render ()
     {
         return (
         <div>
             <h2>Hello There</h2>
-            <p>This page said Mine! Not Yours....</p>
+            <p style={mstyle}>This page said Mine! Not Yours....</p>
+            <p></p>
+            <img src={require(`./me2.jpg`)} />
         </div>
         );
     }
